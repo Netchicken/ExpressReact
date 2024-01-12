7c269fb222f757a2d049c132c83f78b76c684295
Gary <Gary.Dix@enform.nz>
2024-01-12 14:09:48 +1300

    Reactjs added to express

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = 5000; //react runs on 3000
 
 app.get("/", (req, res) => {
-  res.send("Hello from Express!");
+  res.send(
+    "Hello from the Express and ReactJS! this message is held in the server.js file"
+  );
 });
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
